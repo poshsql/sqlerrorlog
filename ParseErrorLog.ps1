b280c60ef0f4cb36588be2f86b7e77423d75db1c
Shiju Samuel <shiju.samuel@gmail.com>
2013-05-06 11:00:23 +0530

    Function to parse errorlog added

--- ParseErrorLog.ps1
+++ ParseErrorLog.ps1
@@ -1,3 +1,28 @@
+function isDate($object) {
+  (($object -as [DateTime]) -is [DateTime])
+}
+function Parse-ErrorEntry(){
+	param(
+      [Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+      [string]$LogEntry
+    )
+	process {
+            if ($LogEntry.Length -gt 35) {
+            $LogObject = @{
+				'ErrorDate' = $LogEntry.Substring(0,22);
+				'ProcessInfo'  = $LogEntry.Substring(23,12);
+				'Message'  = $LogEntry.Substring(35);
+			}
+            if (isDate($LogObject.ErrorDate)) {
+                $obj = New-Object -TypeName PSObject -Property $LogObject
+                $obj.PSObject.typenames.insert(0,'PoshSQL.ErrorLog')
+                write-output $obj | select "ErrorDate","ProcessInfo","Message"
+            }
+            }
+	}
+
+}
 $el = dir log\errorlog* | sort -property LastwriteTime -Descending | select -First 2 
-$el += dir log\errorlog* | sort -property LastwriteTime -Descending | select -First 2 
-$el
+$el | gc| Parse-ErrorEntry 
+
+#Parse-errorEntry "2013-04-26 00:00:44.20 Logon       Login failed for user 'NT AUTHORITY\SYSTEM'. Reason: Failed to open the explicitly specified database. [CLIENT: <local machine>]"
